c1d857314bd59a0504cda4069ab863097f80c38d
Hina0222 <hina0222@naver.com>
2024-10-09 00:45:01 +0900

    save페이지 생성

--- capstone-ej/src/App.js
+++ capstone-ej/src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Decibel from './pages/Decibel';
 import "./styles/style.scss";
+import Save from './components/decibel/Save';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path='/entry' element={<Entry />} />
       <Route path='/about' element={<About />} />
       <Route path='/decibel' element={<Decibel />} />
+      <Route path='/decibel/save' element={<Save />} />
     </Routes>
   );
 }
---
+++ capstone-ej/src/components/decibel/Save.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Save = () => {
+    return (
+        <div className='save-page'>
+            <Link className='home-btn' to="/"></Link>
+        </div>
+    );
+};
+
+export default Save;
