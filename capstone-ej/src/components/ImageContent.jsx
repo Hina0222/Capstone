dae630e930ffecbeaf333143f1b6b112a1f513f6
Hina0222 <hina0222@naver.com>
2024-11-03 16:34:35 +0900

    entry페이지 박스 디자인 수정

--- capstone-ej/src/components/ImageContent.jsx
+++ capstone-ej/src/components/ImageContent.jsx
@@ -1,7 +1,8 @@
 import { useCallback, useMemo, useState, memo } from 'react';
 import { throttle } from 'lodash';
-import Back1 from '../images/EntryImages/Back1.svg';
-import Back2 from '../images/EntryImages/Back2.svg';
+import ContentBox1 from '../images/EntryImages/ContentBox1.png';
+import ContentBox2 from '../images/EntryImages/ContentBox2.png';
+import ContentBox3 from '../images/EntryImages/ContentBox3.png';
 
 const ImageContent = memo(({ content, idx, openModal }) => {
     const adjustedIndex = idx % 9;
@@ -35,22 +36,30 @@ const ImageContent = memo(({ content, idx, openModal }) => {
 
     const backgroundStyle = useMemo(() => {
         const random = Math.random();
-        if (random < 0.1) {
+        if (random < 0.2) {
             return {
-                backgroundImage: `url(${Back1})`,
-                backgroundSize: '50px',
-                backgroundPosition: 'left bottom calc(-1px)',
+                backgroundImage: `url(${ContentBox1})`,
+                backgroundSize: '100% 100%',
                 backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
             };
-        } else if (random < 0.2) {
+        } else if (random < 0.4) {
             return {
-                backgroundImage: `url(${Back2})`,
-                backgroundSize: '140px',
-                backgroundPosition: 'right calc(28px) top calc(-1px)',
+                backgroundImage: `url(${ContentBox2})`,
+                backgroundSize: '100% 100%',
                 backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+            };
+        } else if (random < 0.6) {
+            return {
+                backgroundImage: `url(${ContentBox3})`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+
             };
         }
-        return {};
+        return { border: '1.5px solid #0800EE' };
     }, [idx]);
 
     // 좀 더 알아보고 지우기
@@ -68,8 +77,7 @@ const ImageContent = memo(({ content, idx, openModal }) => {
                 onMouseEnter={MouseEnter}
                 onMouseLeave={MouseLeave}
             >
-                {!isMouseOver ? <img src={`https://bucket-geeks.s3.ap-northeast-2.amazonaws.com/symbol/deactive/${content.category}/${content.id}.svg`} alt="" /> :
-                    <img src={`https://bucket-geeks.s3.ap-northeast-2.amazonaws.com/symbol/active/${content.category}/${content.id}.svg`} alt="" />}
+                <img src={`https://bucket-geeks.s3.ap-northeast-2.amazonaws.com/symbol/deactive/${content.category}/${content.id}.svg`} alt="" />
                 {isMouseOver && (
                     <img
                         src={moveImage}
