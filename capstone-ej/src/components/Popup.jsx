46e5415a9129bd2d5b71db77e797692b17fd970e
Hina0222 <hina0222@naver.com>
2024-11-08 01:02:53 +0900

    팝업 이미지 수정

--- capstone-ej/src/components/Popup.jsx
+++ capstone-ej/src/components/Popup.jsx
@@ -1,10 +1,10 @@
-import PopupImg from '../images/HomeImages/PopupImg.png';
+import PopupGif from '../images/HomeImages/PopupGif.gif';
 import PopupClose from '../images/HomeImages/PopupClose.svg';
 
 const Popup = ({ setPopupOpen }) => {
     return (
         <div className='popup-page'>
-            <img className='popup-img' src={PopupImg} alt="" />
+            <img className='popup-img' src={PopupGif} alt="" />
             <button className='popup-close'>
                 <img onClick={() => { setPopupOpen(false) }} src={PopupClose} alt="" />
             </button>
