2a628120b9e0bdd235080cecebb6346807239b29
Hina0222 <hina0222@naver.com>
2024-11-17 14:09:43 +0900

    about페이지 수정

--- capstone-ej/src/pages/About.jsx
+++ capstone-ej/src/pages/About.jsx
@@ -1,21 +1,10 @@
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
-import Block3 from '../images/AboutImages/Block3.svg';
+import Block3 from '../images/AboutImages/Block3.png';
 import AboutTitle from '../images/AboutImages/AboutTitle.svg';
-import AboutGif1 from '../images/AboutImages/AboutGif1.gif';
-import AboutGif2 from '../images/AboutImages/AboutGif2.gif';
 import { ReactComponent as Home } from '../images/AboutImages/Home.svg';
 
-
 const About = () => {
-    const [showGif, setShowGif] = useState(false);
-    const MouseEnter = () => {
-        setShowGif(true);
-    };
-
-    const MouseLeave = () => {
-        setShowGif(false);
-    };
 
     return (
         <div className='about-page min-h-screen flex flex-col items-center'>
@@ -25,13 +14,10 @@ const About = () => {
             <img className='mx-auto mt-11' src={AboutTitle} alt="" />
             <div className='relative mx-6 -mt-3 test'>
                 <img src={Block3} alt="" />
-                {showGif && <img className='absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2' src={AboutGif2} alt="" />}
             </div>
             <p className='main-text'>
-                골목의 다양한 경고문을 240장 수집하였습니다.<br />
-                골목 거주민들의 분노를 엿볼 수 있는 경고문들을 기준을 세워 분류한 후,<br />
-                경고문의 재미난 요소들에<img onMouseEnter={MouseEnter} onMouseLeave={MouseLeave} src={AboutGif1} alt="" />집중하였습니다.<br />
-                시각으로 보여지는 경고문과 청각으로 들리는 경고문, 두가지 방법으로 거주민들의 분노에 집중해 주세요.
+                이 프로젝트는 주민 생활이 공존하는 골목을 재인식할 필요성을 시사하며,<br/>
+                지역과 주민에 의한 공동체 관계에 중요한 역할을 담당하는 골목의 가치를 재해석하는 데 있다.
             </p>
         </div>
     );
